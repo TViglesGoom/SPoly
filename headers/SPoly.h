06a79b27ccd768ab4bac5147fde117c819f60889
TViglesGoom <tviglesgoom@gmail.com>
2019-11-14 20:09:01 +0300

    make dictionary with hashtable

--- headers/SPoly.h
+++ headers/SPoly.h
@@ -1,6 +1,21 @@
 #ifndef SPOLY_VLADHUZ_SPOLY_H
 #define SPOLY_VLADHUZ_SPOLY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
 
+#define SIZE 20
+
+struct DataItem {
+    int data;
+    int key;
+};
+
+DType hashCode (IType key);
+struct DataItem *search (IType key);
+void insert (IType key, IType data);
+struct DataItem* delete(struct DataItem* item);
+void display();
 
 #endif //SPOLY_VLADHUZ_SPOLY_H
